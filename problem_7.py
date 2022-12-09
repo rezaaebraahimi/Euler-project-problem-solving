c03225d2229fb62fcf2d87e9b951a7b6557cfed0
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-12-09 07:31:29 +0330

    problem n-7 solved

---
+++ problem_7.py
@@ -0,0 +1,27 @@
+"""
+By listing the first six prime numbers: 
+2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+
+"""
+
+def prime(x):
+    for n in range(2, int(x / 2) + 1):
+        if (x % n) == 0:
+            return False
+    else:
+        return True
+
+
+primes = []
+num = 2
+
+while len(primes) < 10001 :
+    if prime(num):
+        primes.append(num)
+    num += 1
+    
+print(primes[-1])
+    ### Answer is 104743 ###
